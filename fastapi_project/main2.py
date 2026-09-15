025304991ca4763b163c111d58ef49067c177dfa
Sujab Kumar Mandal <kumarsujab@gmail.com>
2026-09-15 20:28:58 +0530

    add main2

---
+++ fastapi_project/main2.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+app=FastAPI()
+# list={"india":"hindi",
+# "pak":"urdu","america":"eng","odisha":"odia"}
+# list2=list.keys()
+list=["cloth","food","electronics","furniture","stationary","sports","toys"]
+@app.get("/{lang}")
+def home(lang):
+    if lang not in list:
+        return {"not available"}
+    #return {f"{list.get(lang)} is the main language"}
+    return {"avaliable"}
+# from fastapi import FastAPI
+# @app.get("/seema")
+# def sujab():
+#     return {" i love seema very much"}
