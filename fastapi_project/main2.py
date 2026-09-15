b7d4fa36348948c74c69a2173b935e70180d8001
Sujab Kumar Mandal <kumarsujab@gmail.com>
2026-09-15 23:06:53 +0530

    extra code

--- fastapi_project/main2.py
+++ fastapi_project/main2.py
@@ -3,6 +3,9 @@ app=FastAPI()
 # list={"india":"hindi",
 # "pak":"urdu","america":"eng","odisha":"odia"}
 # list2=list.keys()
+@app.get("/")
+def home():
+    return {"hello world"}
 list=["cloth","food","electronics","furniture","stationary","sports","toys"]
 @app.get("/{lang}")
 def home(lang):
